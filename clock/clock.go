ee56c721872f4529fe38d2c6f7ce805ac53e6e21
zhiruchen <zhiruchen1992@foxmail.com>
2017-09-12 12:54:34 +0800

    add clock solution

--- clock/clock.go
+++ clock/clock.go
@@ -1,8 +1,12 @@
 package clock
 
-const testVersion = 4
+import (
+	"log"
+	"math"
+	"strconv"
+)
 
-// You can find more details and hints in the test file.
+const testVersion = 4
 
 type Clock struct {
 	h int
@@ -13,11 +17,44 @@ func New(hour, minute int) Clock {
 	return Clock{h: hour, m: minute}
 }
 
-func (Clock) String() string {
+func (c Clock) String() string {
+	totalMinutes := c.h*60 + c.m
+	hour := int(math.Floor(float64(totalMinutes)/60)) % 24
+	if hour < 0 {
+		hour += 24
+	}
+	minute := totalMinutes % 60
+	if minute < 0 {
+		minute += 60
+	}
+
+	log.Printf("total minutes: %d\n", totalMinutes)
+	log.Printf("hour: %d, minute: %d\n", hour, minute)
+	rep := ""
+	if hour < 10 {
+		rep += "0"
+	}
+	rep += strconv.FormatInt(int64(hour), 10)
+	rep += ":"
+
+	if minute < 10 {
+		rep += "0"
+	}
+	rep += strconv.FormatInt(int64(minute), 10)
+	log.Printf("%d, %d, %s\n", hour, minute, rep)
+	return rep
 }
 
-func (Clock) Add(minutes int) Clock {
-}
+func (c Clock) Add(minutes int) Clock {
+	totalMinutes := c.h*60 + c.m + minutes
 
-// Remember to delete all of the stub comments.
-// They are just noise, and reviewers will complain.
+	hour := int(math.Floor(float64(totalMinutes)/60)) % 24
+	if hour < 0 {
+		hour += 24
+	}
+	minute := totalMinutes % 60
+	if minute < 0 {
+		minute += 60
+	}
+	return Clock{h: hour, m: minute}
+}
